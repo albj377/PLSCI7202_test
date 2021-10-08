33a12a535d7c06f1b832c45f62d121c60b52e30c
Charlie Hale <chale295@gmail.com>
2021-10-08 15:18:33 -0400

    CH added code comments

--- checking_assumptions.R
+++ checking_assumptions.R
@@ -16,3 +16,10 @@ qqnorm(predictor.lm$residuals) #Checks normalcy
 qqline(predictor.lm$residuals)
 qqPlot(predictor.lm$residuals, main="QQPlot")
 plot(predictor.lm,which=4) #Check influence of outliers.
+
+# CH comments: Looks good! Only suggestion I have is  to add a header to your
+# script files. I started doing this recently myself and have found it helps
+# when going back to old code (or reading someone else's...)
+# I like to include name, the date, the version of the script, what the 
+# script does, and any  input and/or output files.
+
