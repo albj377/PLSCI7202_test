db5c01ce2395724cfa7867cd883b6811777db8e5
Charlie Hale <chale295@gmail.com>
2021-10-12 09:50:48 -0400

    CH added a few more comments

--- checking_assumptions.R
+++ checking_assumptions.R
@@ -16,8 +16,13 @@ qqnorm(predictor.lm$residuals) #Checks normalcy
 qqline(predictor.lm$residuals)
 qqPlot(predictor.lm$residuals, main="QQPlot")
 plot(predictor.lm,which=4) #Check influence of outliers.
-
-# CH comments: Looks good! Only suggestion I have is  to add a header to your
+ing
+# CH comments: Looks good! Comments are used effectively, variable names make
+# sense, code is easy to understand. I don't have the data needed to run this,
+# but looks like it should work. Small nitpick: I find it easier to read code
+# with more spaces incorporated, e.g. "Age <- Data$Age". I see lots of people
+# go without spaces, but I prefer the look of it better with spaces in there.
+# Another thing I've found helpful is add a header to
 # script files. I started doing this recently myself and have found it helps
 # when going back to old code (or reading someone else's...)
 # I like to include name, the date, the version of the script, what the 
